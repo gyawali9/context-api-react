cc88b63d2978695535e7b7c54c5ae773c3408012
roshanspanilk <roshang@spanilk.com>
2021-12-17 19:01:09 +0545

    header section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Header from "../src/components/Header";
+import Cart from "./components/Cart";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
+      {/* <main> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+      {/* </main> */}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+import styles from "./styles.css";
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <div>
+      <span clasName="header">React Context API Tut</span>
+      <ul className="nav">
+        <li>
+          <Link to="/">Home Page</Link>
+        </li>
+        <li>
+          <Link to="/cart">Cart Page</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Header;
