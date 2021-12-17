ebf8807fced386dec799f3a452d3a0f3d7916099
roshanspanilk <roshang@spanilk.com>
2021-12-17 19:27:09 +0545

    code refinement

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import styles from "./styles.css";
 const Header = () => {
   return (
     <div>
-      <span clasName="header">React Context API Tut</span>
+      <span clasName="header">React Context API Operation</span>
       <ul className="nav">
         <li>
           <Link to="/">Home Page</Link>
